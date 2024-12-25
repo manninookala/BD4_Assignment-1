ccedd1026d9432cb35e1c81104584f039e95495f
manninookala <nookalamanni2509@gmail.com>
2024-12-25 22:38:40 +0530

    path changes

--- BD4_Assignment1/initDB.js
+++ BD4_Assignment1/initDB.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./BD4_Assignment1/database.sqlite', (err) => {
+const db = new sqlite3.Database('/home/manninookala/BD4_Assignment-1/BD4_Assignment1/index.js', (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
